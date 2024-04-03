7166c32fd3c14018744424335c05b015a484af4c
Ism1tha <xgalan@hotmail.com>
2024-04-03 12:10:08 +0200

    Primera part del projecte

---
+++ Projecte UF5 MP03/src/main/java/com/insebre/project/controller/ExceptionController.java
@@ -0,0 +1,11 @@
+package com.insebre.project.controller;
+
+import com.insebre.project.exception.CustomException;
+
+public class ExceptionController {
+
+    public void throwCustomException() throws CustomException {
+        // Some logic that may throw the custom exception
+        throw new CustomException("This is a custom exception message.");
+    }
+}
