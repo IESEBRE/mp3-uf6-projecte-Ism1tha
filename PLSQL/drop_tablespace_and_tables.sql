a3b3949f18db9cb4fcd75609bd8905baf3807715
Ism1tha <xgalan@hotmail.com>
2024-05-18 20:02:52 +0200

    Drop queries on the sql file

---
+++ PLSQL/drop_tablespace_and_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE programs_user.programs;
+DROP TABLE programs_user.versions;
+DROP SEQUENCE programs_user.programs_seq;
+DROP SEQUENCE programs_user.versions_seq;
+DROP USER programs_user CASCADE;
+DROP TABLESPACE programs_tablespace INCLUDING CONTENTS AND DATAFILES;
